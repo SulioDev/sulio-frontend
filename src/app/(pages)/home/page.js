b8589432956f770e5bc1619aa6ea1352262f1244
Aadi563 <136591469+Aadi563@users.noreply.github.com>
2025-01-06 14:52:41 +0530

    remove video introduction

--- src/app/(pages)/home/page.js
+++ src/app/(pages)/home/page.js
@@ -2,7 +2,6 @@ import LandingPage from "@/app/components/Home/FinalCallToActionSection/LandingP
 import FAQSection from "@/app/components/Home/FrequentlyAskedQuestionsSection/FAQSection";
 import Hero from "@/app/components/Home/HeroSection/Hero";
 import MarketingStats from "@/app/components/Home/TestimonialsSection/MarketingStats";
-import VideoIntroduction from "@/app/components/Home/VideoIntroductionSection/VideoIntroduction";
 
 export default function Home() {
   return (
@@ -10,7 +9,6 @@ export default function Home() {
       <Hero/>
       <MarketingStats/>
       <FAQSection/>
-      {/* <VideoIntroduction/> */}
       <LandingPage/>
     </main>
   );
