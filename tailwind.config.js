b61e5d480833f9ee72431659d45957a28f150106
Aadi563 <136591469+Aadi563@users.noreply.github.com>
2025-01-10 13:13:04 +0530

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-<<<<<<< HEAD
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-=======
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/components/**/*.{js,ts,jsx,tsx,mdx}",
->>>>>>> 3102cd1b0cea704e79bc98cd66588ff803a5f766
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
